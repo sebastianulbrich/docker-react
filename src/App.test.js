9f5e969e03d4c58366d68f822fafd2a8ccef84f3
Sebastian Ulbrich <Sebastian.Ulbrich@concat.de>
2020-09-11 08:50:56 +0200

    added travis file

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText('react');
-  expect(linkElement).toBeInTheDocument();
+
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+  });
 });
